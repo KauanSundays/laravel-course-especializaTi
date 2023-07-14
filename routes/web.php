6a2a26bca7a57b21956408b144670df61fda1688
Kauan Sundays <102335585+KauanSundays@users.noreply.github.com>
2023-07-14 11:53:53 -0300

    feat: "Explicações com detalhes sobre as rotas"

--- routes/web.php
+++ routes/web.php
@@ -11,3 +11,12 @@ Route::get('/', function () {
 
 // Rota para a página de contato, usando o controlador SiteController
 Route::get('/contato', [SiteController::class, 'contact']);
+//Route::get: É um método do Laravel que define uma rota HTTP GET. Isso significa que essa rota responderá apenas a solicitações GET feitas para a URL especificada.
+
+//'/contato': É o URL (endereço) da rota. Nesse caso, a rota corresponde à URL '/contato'. Quando um usuário acessar essa URL, a função correspondente será executada.
+
+//[SiteController::class, 'contact']: É uma representação de um controlador e um método que serão executados quando a rota for acessada. Nesse caso, o controlador é SiteController e o método é contact.
+
+// SiteController::class: Refere-se à classe SiteController, que é responsável por controlar a lógica relacionada às ações do site.
+
+// 'contact': É o nome do método no controlador SiteController que será executado quando a rota for acessada. No caso específico, o método contact será chamado.
