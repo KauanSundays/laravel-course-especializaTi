abade22370a4549d30f836dae191b5b7c2c2b4c2
Kauan Sundays <102335585+KauanSundays@users.noreply.github.com>
2023-07-17 19:33:11 -0300

    feat: "clean code"

--- resources/views/admin/supports/index.blade.php
+++ resources/views/admin/supports/index.blade.php
@@ -1,3 +1,24 @@
 <h1>Listagem das duvidas</h1>
 
 <a href="{{ route('supports.create') }}" class="btn btn-primary">Ir para a Bio</a> <!--create button-->
+
+
+<table>
+    <thead>
+        <th>Subject</th>
+        <th>Status</th>
+        <th>Description</th>
+        <th></th>
+    </thead>
+
+    <tbody>
+        @foreach ($supports as $support)
+            <tr>
+                <td>{{ $support->subject }}</td>
+                <td>{{ $support->status }}</td>
+                <td>{{ $support->body }}</td>
+                <td></td>
+            </tr>
+        @endforeach
+    </tbody>
+</table>
