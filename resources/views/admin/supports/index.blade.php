e6f6d8514ff91c09be6833a00624f65693ff4bf8
Kauan Sundays <102335585+KauanSundays@users.noreply.github.com>
2023-07-21 14:38:40 -0300

    feat: "WIP"

--- resources/views/admin/supports/index.blade.php
+++ resources/views/admin/supports/index.blade.php
@@ -1,6 +1,9 @@
 <h1>Listagem das duvidas</h1>
 
-<a href="{{ route('supports.create') }}" class="btn btn-primary">Ir para a Bio</a> <!--create button-->
+<a href="{{ route('supports.create') }}" 
+class="btn btn-primary">
+    Criar
+</a> <!--create button-->
 
 
 <table>
